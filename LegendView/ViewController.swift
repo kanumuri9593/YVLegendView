56d74d12fd587f1b99645d34e7a37de6f629b6f9
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-07-27 11:17:51 -0400

    Screen Shots

--- LegendView/ViewController.swift
+++ LegendView/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIImage(named: <#T##String#>)
         AddLegendView(LegendInfoFeed:MultipleLegendFeed)
     }
     
